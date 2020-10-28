c1a9c412966224f1051c33c9c7a6054fffb08b77
ABMARK43 <kumaransh372@gmail.com>
2020-10-28 18:30:29 +0530

    initial commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const uuid = require('uuid');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 const sessionId = uuid.v4();
 
 
